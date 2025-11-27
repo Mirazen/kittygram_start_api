926baa3b226ee76ef47923c11dfdd8abd54b8611
Mirazen <danya-galenko@yandex.ru>
2025-11-27 14:28:08 +0300

    add serializer

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import Cat, Owner
+from .models import Cat, Owner, Achievement
 
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
 
 class CatSerializer(serializers.ModelSerializer):
-    
+    owner = serializers.StringRelatedField(read_only=True)
+    achievements = serializers.StringRelatedField(read_only=True, many=True)
+
     class Meta:
         model = Cat
         fields = ('name', 'color', 'birth_year', 'owner')
@@ -12,6 +19,7 @@ class CatSerializer(serializers.ModelSerializer):
 
 class OwnerSerializer(serializers.ModelSerializer):
     cats = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Owner
         fields = ('first_name', 'last_name', 'cats')
